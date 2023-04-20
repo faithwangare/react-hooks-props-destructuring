7c1e2f8a56176e47844a89b3c320895b9e651c58
Georgina <faithwangare750@gmail.com>
2023-04-20 10:07:30 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import MovieCard from "./MovieCard";
 function App() {
   const title = "Mad Max";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
-
+  const posterURL ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc = {posterURL }genres={genresArr} />
     </div>
   );
 }
